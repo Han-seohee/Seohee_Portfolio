853e5b5750d51df9f71c74a83fecc91caa55f657
Hanseohee <a67114585a@gmail.com>
2021-11-10 21:18:35 +0900

    btn

--- src/Components/Covid/Covid.js
+++ src/Components/Covid/Covid.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import { Button } from 'react-bootstrap'
 import axios from 'axios';
 import React from 'react';
 
@@ -7,6 +8,8 @@ function Covid() {
     const key = 'H+n33W2WWhRLY358pYUSI7utTn5NMjnA5wGhh3fOvjDu3Dr+WPllB9jLaLaHWQJXZVnwB1rqCn637HKz1aUrcQ==';
     const [data, setData] = useState([]);
     const [display, setDisplay] = useState(false);
+    const [btnGroup, setbtnGroup] = useState([]);
+    const [filterIndex, setfilterIndex] = useState('');
 
     useEffect(() => {
         call_Api();
@@ -26,18 +29,60 @@ function Covid() {
         .then((response) => {
             console.log(response);
             console.log(response.data.response.body.items);
-            setData(response.data);
+
+            let temp = [];
+            response = response.data.response.body.items.item;
+
+            if(response.length > 0) {
+                response.map(row => {
+                    temp.push(row.gubun);
+                });
+
+                if(filterIndex.length > 0) {
+                    setData(
+                        response.filter(row => row.gubun === filterIndex)
+                    );
+                }   else {
+                    setData(response);
+                }
+            }
+
+            setbtnGroup(temp);
             setDisplay(true);
         })
         .catch((error) => {
             console.log(error);
             setDisplay(false);
-        })
+        });
     }
     
     return(
         <>
         코로나19 차트
+        { btnGroup.length > 0 ?
+        btnGroup.map((gubun, index) =>
+            <Button
+                className="mb-2"
+                variant="secondary"
+                value={gubun}
+                key={index}
+                size="lg"
+                active>{gubun}
+            </Button>
+            ):
+            <div>데이터가 존재하지 않습니다.</div>
+        }
+            <>
+                { data ? data.map((row, index) => {
+                    return(
+                        <div key={index}>
+                            {row.gubun} , {row.gubunEn}, {row.isolClearCnt}
+                        </div>
+                    )
+                }):
+                    <div></div>
+                }
+            </>
         </>
     )
 }
--- src/Components/Covid/CovidButton.js
+++ src/Components/Covid/CovidButton.js
@@ -1,24 +1,24 @@
-import React, { useState, useEffect } from 'react';
-import { Button } from 'react-bootstrap';
+// import React, { useState, useEffect } from 'react';
+// import { Button } from 'react-bootstrap';
 
-function CovidBtn(props) {
-    return(
-        <>
-        {props.btnIndex ? props.btnIndex.map((gubun, index) => {
-            console.log(gubun, index);
-            <Button
-                className="mb-2"
-                variant="secondary"
-                value={gubun}
-                key={index}
-                size="lg"
-                active>{gubun}
-            </Button>
-        }):
-        <div>데이터가 존재하지 않습니다.</div>
-        }
-        </>
-    )   
-}
+// function CovidBtn(props) {
+//     return(
+//         <>
+//         {props.btnIndex ? props.btnIndex.map((gubun, index) => {
+//             console.log(gubun, index);
+//             <Button
+//                 className="mb-2"
+//                 variant="secondary"
+//                 value={gubun}
+//                 key={index}
+//                 size="lg"
+//                 active>{gubun}
+//             </Button>
+//         }):
+//         <div>데이터가 존재하지 않습니다.</div>
+//         }
+//         </>
+//     )   
+// }
 
-export default CovidBtn;
+// export default CovidBtn;
